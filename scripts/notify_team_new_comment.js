5101832f4c32db0afce8c45239a1b9293973970a
Michaela Robosova <robosovam@gmail.com>
2025-07-03 12:17:36 +0200

    PR review

--- scripts/notify_team_new_comment.js
+++ scripts/notify_team_new_comment.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { LE_BOT_USERNAME, BOT_MESSAGE } = require('./constants');
+// TODO REVIEW: Can be text file too?
 const { CLOSE_CONTRIBUTORS } = require('./close_contributors');
 
 module.exports = async ({ github, context, core }) => {
@@ -13,19 +14,36 @@ module.exports = async ({ github, context, core }) => {
     const commentTime = new Date(context.payload.comment.created_at);
     const oneHourBefore = new Date(commentTime - 3600000);
     const commentAuthor = context.payload.comment.user.login;
+    // TODO REVIEW: Ask Richard for security recommendations since anyone can comment on an issue
     const commentBody = context.payload.comment.body;
     const repo = context.repo.repo;
     const owner = context.repo.owner;
+    // TODO REVIEW: Rename 'slackWebhookUrl' and 'communityWebhookUrl'
     const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
     const communityWebhookUrl = process.env.SLACK_COMMUNITY_NOTIFICATIONS_WEBHOOK_URL;
     const keywordsPath = path.join(__dirname, 'keywords.txt');
+    // TODO REVIEW: Cleanup duplicate keywords
     const keywordRegexes = fs.readFileSync(keywordsPath, 'utf-8')
       .split('\n')
       .map(k => k.trim().toLowerCase())
       .filter(Boolean)
       .map(keyword => new RegExp(`\\b${keyword}\\b`, 'i'));
 
-
+    /* console.log('issueNumber:', issueNumber);
+    console.log('issueUrl:', issueUrl);
+    console.log('issueTitle:', issueTitle);
+    console.log('escapedTitle:', escapedTitle);
+    console.log('commentId:', commentId);
+    console.log('commentTime:', commentTime);
+    console.log('oneHourBefore:', oneHourBefore);
+    console.log('commentAuthor:', commentAuthor);
+    console.log('commentBody:', commentBody);
+    console.log('repo:', repo);
+    console.log('owner:', owner);
+    console.log('slackWebhookUrl:', slackWebhookUrl ? '[set]' : '[not set]');
+    console.log('communityWebhookUrl:', communityWebhookUrl ? '[set]' : '[not set]');
+    console.log('keywordsPath:', keywordsPath);
+    console.log('keywordRegexes:', keywordRegexes.map(r => r.toString())); */
 
     async function hasLabel(name) {
         let labels = [];
@@ -68,6 +86,7 @@ module.exports = async ({ github, context, core }) => {
               body: `Hi @${commentAuthor} ${BOT_MESSAGE}`
             });
             if (response?.data?.html_url) {
+              // TODO REVIEW: Rename 'bot_replied' and 'botReplyMessage'
               core.setOutput('bot_replied', true);
               const botReplyMessage = `*[${repo}] <${response.data.html_url}|Bot response sent> on issue: <${issueUrl}|${escapedTitle}>*`;
               core.setOutput('bot_reply_message', botReplyMessage);
@@ -78,7 +97,7 @@ module.exports = async ({ github, context, core }) => {
         }
         return response;
     }
-
+    // TODO: Continue review here
 
     if (await hasLabel('help wanted') || CLOSE_CONTRIBUTORS.includes(commentAuthor)) {
       core.setOutput('webhook_url', slackWebhookUrl);
