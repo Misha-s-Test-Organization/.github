242338f019387b86d319ec414cf1e8e328e99f9a
iamshobhraj <iamshobhraj@gmail.com>
2025-09-06 00:49:09 +0530

    Fix: Use correct input variable in action and script
    The input variable name was incorrect in the GitHub Action workflow file and the
    JavaScript script. This commit fixes this issue by using the correct variable
    name.

--- scripts/contributor-issue-comment.js
+++ scripts/contributor-issue-comment.js
@@ -20,7 +20,7 @@ module.exports = async ({ github, context, core }) => {
     const owner = context.repo.owner;
     const supportDevSlackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
     const supportDevNotificationsSlackWebhookUrl = process.env.SLACK_COMMUNITY_NOTIFICATIONS_WEBHOOK_URL;
-    const isCloseContributor = core.getBooleanInput('is-close-contributor');
+    const isCloseContributor = core.getBooleanInput('IS-CLOSE-CONTRIBUTOR');
     const keywordRegexes = KEYWORDS_DETECT_ASSIGNMENT_REQUEST
       .map(k => k.trim().toLowerCase())
       .filter(Boolean)
